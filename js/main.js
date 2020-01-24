7e646983a722c6eb0c86769266e77f03cc410146
Anton Webb <anton25360@gmail.com>
2020-01-24 11:53:04 +0000

    Added 'for' attributes in labels

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('hello lol');
+function authenticateUser() {
+
+    
+
+    let username = document.getElementById('userInput').value
+    let password = document.getElementById('pwInput').value
+
+    console.log(username);
+    console.log(password);
+    
+    
+}
