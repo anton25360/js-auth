1b4fae6c86a8174af3a4941d1d18ed7d37d34345
Anton Webb <anton25360@gmail.com>
2020-01-27 17:52:46 +0000

    Removed comments

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,6 @@
 function signupFunction(event) {
     event.preventDefault()
 
-    // window.location.replace("/account.html");
-
     let form = document.querySelector('#signupForm')
 
     let email = form['signupEmail'].value
