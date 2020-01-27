562387be1a175d7f14db6362a1f3488778e91cac
Anton Webb <anton25360@gmail.com>
2020-01-27 17:07:48 +0000

    improved getting values directly from form

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function getCredentials(event) {
-
     event.preventDefault()
-
-    let username = document.getElementById('usernameInput').value
-    let password = document.getElementById('passwordInput').value
-
-    console.log(username);
-    console.log(password);
        
     // window.location.replace("/account.html");
 
     const form = document.querySelector('#loginForm')
+
+    const user = form['loginUsername'].value
+    const pass = form['loginPassword'].value
+
+    console.log(user, pass);  
+
+    firebase
 
 }
