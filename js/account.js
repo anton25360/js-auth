25b29e8d169521f912ec9a32649c355e39faea38
Anton Webb <anton25360@gmail.com>
2020-01-27 18:23:56 +0000

    Added custom account message

--- js/account.js
+++ js/account.js
@@ -1,5 +1,8 @@
 auth.onAuthStateChanged(function(user) {
     if (user) {
         document.querySelector('#emailTarget').innerHTML = user.email
+        document.querySelector('#messageTarget').innerHTML = 'You are now logged in.'
     }
 })
+
+//todo: make logout work, add account error messages if user inst loggied in, fix logs and alerts.
