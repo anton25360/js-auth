d0a078ad6e9afd2aedc29c7c8232504d1ac9bcb2
Anton Webb <anton25360@gmail.com>
2020-01-29 13:43:13 +0000

    link fix

--- js/account.js
+++ js/account.js
@@ -12,7 +12,7 @@ auth.onAuthStateChanged(function(user) {
 
 function logout() {
     auth.signOut().then(()=>{
-        window.location.replace("/");
+        window.location.replace("/js-auth/");
     })
     
 }
