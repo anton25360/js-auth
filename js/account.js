5ae6a2efd2700c93f8b4c31f186fd1a82604096d
Anton Webb <anton25360@gmail.com>
2020-01-29 13:16:14 +0000

    made logout button work

--- js/account.js
+++ js/account.js
@@ -2,7 +2,16 @@ auth.onAuthStateChanged(function(user) {
     if (user) {
         document.querySelector('#emailTarget').innerHTML = user.email
         document.querySelector('#messageTarget').innerHTML = 'You are now logged in.'
-    }
+
+    } 
 })
+
+function logout() {
+    console.log('go away');
+    auth.signOut().then(()=>{
+        window.location.replace("/");
+    })
+    
+}
 
 //todo: make logout work, add account error messages if user inst loggied in, fix logs and alerts. fix signu btn on submit
