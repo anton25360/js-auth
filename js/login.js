9b2e3839abd8baa0850ff2a5629556e26ead0c89
Anton Webb <anton25360@gmail.com>
2020-02-18 20:03:53 +0000

    added google auth function

--- js/login.js
+++ js/login.js
@@ -10,12 +10,12 @@ function loginFunction(event) {
         .catch(error => {
 
             alert(error.message);
-            
+
         })
 
         .then(cred => {
-            
-            if (cred == null) {} else {
+
+            if (cred == null) { } else {
                 alert('Success! You are now logged in.')
                 window.location.replace("account.html");
             }
@@ -23,5 +23,24 @@ function loginFunction(event) {
 }
 
 function loginFunctionGoogle() {
-    alert('google!')
+    // alert('google!')
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        //redirect...
+        window.location.replace("account.html");
+        
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
 }
